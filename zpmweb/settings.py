8a4ea5f962f306455165e9fbcea93c6d6e0b4c62
kwaiidev <saceriojason@gmail.com>
2026-02-05 20:47:17 -0500

    last render fix i swear

--- zpmweb/settings.py
+++ zpmweb/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-jaa(#(386&hp1x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', 'localhost,127.0.0.1,zpmweb.onrender.com').split(',')
 
 
 # Application definition
