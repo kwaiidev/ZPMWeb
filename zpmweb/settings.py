557decc9f2169f083b2bfff8cd71f68fc349f6e7
kwaiidev <saceriojason@gmail.com>
2026-02-05 20:28:19 -0500

    render STOP

--- zpmweb/settings.py
+++ zpmweb/settings.py
@@ -117,3 +117,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.environ.get('DJANGO_STATIC_ROOT', str(BASE_DIR / 'staticfiles'))
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
